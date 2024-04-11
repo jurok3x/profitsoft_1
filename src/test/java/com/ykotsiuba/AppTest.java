220574cf0a85b66eaf69eb973b1233e71301e57c
ykotsiuba <jurok3x@gmail.com>
2024-04-11 17:21:00 +0300

    Added tests.

--- src/test/java/com/ykotsiuba/AppTest.java
+++ src/test/java/com/ykotsiuba/AppTest.java
@@ -1,38 +1,21 @@
 package com.ykotsiuba;
 
-import junit.framework.Test;
+import com.ykotsiuba.configuration.TestFilesGenerator;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testApp() {
+        TestFilesGenerator generator = new TestFilesGenerator();
+        for (int i = 0; i < 10; i++) {
+            generator.write(100);
+        }
     }
 }
