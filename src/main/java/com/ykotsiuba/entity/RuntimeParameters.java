bd3c5b6f8031dc13677604f664016ce8cbcbeefb
ykotsiuba <jurok3x@gmail.com>
2024-04-12 16:08:13 +0300

    Updated RuntimeParameters added errors for incorrect parameters.

--- src/main/java/com/ykotsiuba/entity/RuntimeParameters.java
+++ src/main/java/com/ykotsiuba/entity/RuntimeParameters.java
@@ -1,5 +1,7 @@
 package com.ykotsiuba.entity;
 
+import com.ykotsiuba.utils.FileSystemUtils;
+import com.ykotsiuba.utils.ReflectionUtils;
 import lombok.Getter;
 
 @Getter
@@ -12,10 +14,37 @@ public class RuntimeParameters {
     private String parameterName;
 
     public RuntimeParameters(String[] args) {
-        if (args == null || args.length < REQUIRED_ARGS_COUNT || args[0] == null || args[1] == null) {
-            throw new RuntimeException("Runtime parameters not provided or insufficient.");
-        }
+        validateArgs(args);
         this.folderPath = args[0];
         this.parameterName = args[1];
+    }
+
+    private boolean validateArgs(String[] args) {
+        if (args == null || args.length < REQUIRED_ARGS_COUNT) {
+            throw new IllegalArgumentException("Runtime parameters not provided or insufficient.");
+        }
+        if(!validatePath(args)) {
+            throw new IllegalArgumentException("Path is not correct.");
+        }
+        if(!validateParameter(args)) {
+            throw new IllegalArgumentException("Parameter is not correct.");
+        }
+        return true;
+    }
+
+    private boolean validatePath(String[] args) {
+        if (args[0] == null) {
+            return false;
+        }
+        String path = args[0];
+        return FileSystemUtils.isValidPath(path);
+    }
+
+    private boolean validateParameter(String[] args) {
+        if (args[1] == null) {
+            return false;
+        }
+        String parameter = args[1];
+        return ReflectionUtils.contains(parameter);
     }
 }
