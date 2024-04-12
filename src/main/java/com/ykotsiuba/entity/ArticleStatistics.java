2c410cee4299aab1c992c4d72121db5444b697d8
ykotsiuba <jurok3x@gmail.com>
2024-04-12 16:08:47 +0300

    Updated statistic entities.

--- src/main/java/com/ykotsiuba/entity/ArticleStatistics.java
+++ src/main/java/com/ykotsiuba/entity/ArticleStatistics.java
@@ -2,13 +2,17 @@ package com.ykotsiuba.entity;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Setter
+@JacksonXmlRootElement(localName = "statistics")
 public class ArticleStatistics {
 
-    @JacksonXmlElementWrapper(localName = "statistics")
+    @JacksonXmlElementWrapper(useWrapping = false)
     @JacksonXmlProperty(localName = "item")
     private List<StatisticsItem> statistics;
 
--- src/main/java/com/ykotsiuba/entity/StatisticsItem.java
+++ src/main/java/com/ykotsiuba/entity/StatisticsItem.java
@@ -1,11 +1,14 @@
 package com.ykotsiuba.entity;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Builder;
 
-@JacksonXmlRootElement(localName = "library")
+@JacksonXmlRootElement(localName = "item")
 @Builder
 public class StatisticsItem {
+    @JacksonXmlElementWrapper(localName = "value")
     private String value;
+    @JacksonXmlElementWrapper(localName = "count")
     private Integer count;
 }
