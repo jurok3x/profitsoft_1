9caea184989fd9e8cb4018147f646ba8c3fb9484
ykotsiuba <jurok3x@gmail.com>
2024-04-17 13:07:23 +0300

    Added javadocs to writer.

--- src/main/java/com/ykotsiuba/service/impl/ArticleWriterImpl.java
+++ src/main/java/com/ykotsiuba/service/impl/ArticleWriterImpl.java
@@ -12,6 +12,10 @@ import com.ykotsiuba.service.ArticleWriter;
 import java.io.File;
 import java.util.List;
 
+/**
+ * The ArticleWriterImpl class implements the ArticleWriter interface
+ * and provides functionality to write article statistics to an XML file.
+ */
 public class ArticleWriterImpl implements ArticleWriter {
     private final ConcurrentParameterMap parameterMap;
 
@@ -22,6 +26,12 @@ public class ArticleWriterImpl implements ArticleWriter {
         this.parameters = parameters;
     }
 
+    /**
+     * Writes article statistics to an XML file.
+     * The XML file name is generated based on the parameter name from runtime parameters.
+     * Statistics are obtained from the parameter map and mapped to StatisticsItem objects,
+     * which are then written to the XML file using Jackson's XML serialization.
+     */
     public void write() {
         String name = String.format("statistics_by_%s.xml", parameters.getParameterName());
         List<StatisticsItem> statisticsItems = mapStatistics();
@@ -30,6 +40,12 @@ public class ArticleWriterImpl implements ArticleWriter {
         writeXML(statistics, name);
     }
 
+    /**
+     * Writes the given ArticleStatistics object to an XML file with the specified file name.
+     *
+     * @param statistics The article statistics to write.
+     * @param fileName   The name of the XML file.
+     */
     private void writeXML(ArticleStatistics statistics, String fileName) {
         try {
             ObjectMapper xmlMapper = new XmlMapper();
@@ -42,6 +58,11 @@ public class ArticleWriterImpl implements ArticleWriter {
         }
     }
 
+    /**
+     * Maps article statistics from the parameter map to StatisticsItem objects.
+     *
+     * @return A list of StatisticsItem objects representing article statistics.
+     */
     private List<StatisticsItem> mapStatistics() {
         return parameterMap.getStatistics()
                 .entrySet()
